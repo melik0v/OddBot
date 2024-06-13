5011e83ad0810d0f6ecef4750dfe6a0fd9a017b9
melik0v <melik0v@domain.com>
2024-06-13 10:58:23 +0000

    Add some base functionality

---
+++ handlers/start.py
@@ -0,0 +1,17 @@
+from aiogram import Router, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+
+start_router = Router()
+
+@start_router.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer('Привет!🙌 Я буду звать тебя пить пивко🍻 Создатель ещё лепит меня из 💩 и 🪵, поэтому обожди какое-то время😉')
+
+@start_router.message(Command('start_2'))
+async def cmd_start_2(message: Message):
+    await message.answer('Запуск сообщения по команде /start_2 используя фильтр Command()')
+
+@start_router.message(F.text == '/start_3')
+async def cmd_start_3(message: Message):
+    await message.answer('Запуск сообщения по команде /start_3 используя магический фильтр F.text!')
